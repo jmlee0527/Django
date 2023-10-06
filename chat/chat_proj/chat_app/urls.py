55dfa0093644be1286866c751e704d5f527d8c37
jmlee0527 <wjdalsskt@naver.com>
2023-10-06 22:54:40 +0900

    feat :채팅 서버 구현

---
+++ chat/chat_proj/chat_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('',views.index, name = 'index'),
+    path("<str:room_name>/<str:username>/",views.room,name = 'room'),
+]
